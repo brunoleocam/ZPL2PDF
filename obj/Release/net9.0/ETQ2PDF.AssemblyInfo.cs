16da391d9b2476b7423c11d3d4b74b0bb571e48f
brunoleocam <brunoleocam@gmail.com>
2025-02-06 17:00:46 -0300

    Update AssemblyInfo and add Source Link support for better version tracking

--- obj/Release/net9.0/ETQ2PDF.AssemblyInfo.cs
+++ obj/Release/net9.0/ETQ2PDF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETQ2PDF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46703015d00efab6b9a5a9a8e3ab1efecde5cb80")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETQ2PDF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETQ2PDF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
