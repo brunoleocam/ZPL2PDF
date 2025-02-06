5c2bccdc64c0f00c0abda7da0c9ba524bd361645
brunoleocam <brunoleocam@gmail.com>
2025-02-06 17:36:35 -0300

    Update binaries and assembly information for ZPL2PDF

--- obj/Debug/net9.0/ZPL2PDF.AssemblyInfo.cs
+++ obj/Debug/net9.0/ZPL2PDF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZPL2PDF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23adf10725a03a23e23a7a4c89188468be8114c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505c1dfa180e744f2c8f0960243d09c26bff1f4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZPL2PDF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZPL2PDF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
